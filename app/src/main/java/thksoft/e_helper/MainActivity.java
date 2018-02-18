fe7421748baa44bdf2a42aeff91e743e63f5c976
Truong Nguyen <Truong.Nguyen@transtech.net.au>
2018-02-19 09:28:17 +1100

    Support read each question and answer

--- app/src/main/java/thksoft/e_helper/MainActivity.java
+++ app/src/main/java/thksoft/e_helper/MainActivity.java
@@ -2,6 +2,8 @@ package thksoft.e_helper;
 
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.UtteranceProgressListener;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -21,14 +23,17 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     TextToSpeech tts;
-    EditText ed1;
+    TextView tvQuestion, tvAnswer;
     Button b1, b2, b3, bStop;
-    CheckBox selectAll, repeat;
+    CheckBox selectAll, repeat, showAnswer, chRandomQ, chRealTest;
     String[] toSpeak = new String[]{};
     String[] text = new String[]{};
     int sPart = 0;
     int fileIdx = 0;
     int lineIdx = 0;
+    boolean showExample = false, choseRandomly = false, isRealTest = false;
+    Handler mHandler = null;
+    int curFileIndex = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +44,11 @@ public class MainActivity extends AppCompatActivity {
         bStop = findViewById(R.id.button_stop);
         selectAll = findViewById(R.id.checkBox_all);
         repeat = findViewById(R.id.checkBox_repeat);
+        showAnswer = findViewById(R.id.cbShowAnswer);
+        chRandomQ = findViewById(R.id.randomQ);
+        chRealTest = findViewById(R.id.chkRealTest);
+        tvQuestion = findViewById(R.id.tvQuestion);
+        tvAnswer = findViewById(R.id.tvAnswer);
 
         tts=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -78,6 +88,14 @@ public class MainActivity extends AppCompatActivity {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                toSpeak = new String[]{};
+                text = new String[]{};
+                sPart = 1;
+                fileIdx = 0;
+                lineIdx = 0;
+                showExample = showAnswer.isChecked();
+                choseRandomly = chRandomQ.isChecked();
+                isRealTest = chRealTest.isChecked();
                 if (selectAll.isChecked()) {
                     toSpeak = getAllFile("Part1");
                 } else {
@@ -86,9 +104,6 @@ public class MainActivity extends AppCompatActivity {
                 if (toSpeak.length == 0) {
                     Toast.makeText(getApplicationContext(), "No file!", Toast.LENGTH_SHORT).show();
                 }
-                sPart = 1;
-                fileIdx = 0;
-                lineIdx = 0;
                 speakOut();
             }
         });
@@ -96,6 +111,14 @@ public class MainActivity extends AppCompatActivity {
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                toSpeak = new String[]{};
+                text = new String[]{};
+                sPart = 2;
+                fileIdx = 0;
+                lineIdx = 0;
+                showExample = showAnswer.isChecked();
+                choseRandomly = chRandomQ.isChecked();
+                isRealTest = chRealTest.isChecked();
                 if (selectAll.isChecked()) {
                     toSpeak = getAllFile("Part2");
                 } else {
@@ -104,12 +127,27 @@ public class MainActivity extends AppCompatActivity {
                 if (toSpeak.length == 0) {
                     Toast.makeText(getApplicationContext(), "No file!", Toast.LENGTH_SHORT).show();
                 }
-                sPart = 2;
-                fileIdx = 0;
-                lineIdx = 0;
                 speakOut();
             }
         });
+
+        mHandler = new Handler(android.os.Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message message) {
+                if (message.what % 2 == 0) {
+                    if (showExample) {
+                        tvQuestion.setText(message.obj.toString());
+                        tvAnswer.setText("");
+                    } else {
+                        tvQuestion.setText("");
+                        tvAnswer.setText("");
+                    }
+                } else {
+                    if (showExample) tvAnswer.setText(message.obj.toString());
+                    else tvAnswer.setText("");
+                }
+            }
+        };
     }
 
     @Override
@@ -144,8 +182,20 @@ public class MainActivity extends AppCompatActivity {
                     HashMap<String, String> map = new HashMap<String, String>();
                     map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, String.valueOf(fileIdx)
                             + String.valueOf(lineIdx));
-                    tts.speak(Utilities.ExtractContent(text[lineIdx++]), TextToSpeech.QUEUE_FLUSH, map);
-                    if (lineIdx == text.length) fileIdx++;
+                    String sText = Utilities.ExtractContent(text[lineIdx]);
+                    tts.speak(sText, TextToSpeech.QUEUE_FLUSH, map);
+                    Message message = mHandler.obtainMessage(lineIdx, sText);
+                    message.sendToTarget();
+                    if (isRealTest) lineIdx += 2;
+                    else lineIdx++;
+                    if (lineIdx >= text.length) {
+                        if (choseRandomly) {
+                            Random r = new Random();
+                            fileIdx = r.nextInt(toSpeak.length - 1);
+                        } else {
+                            fileIdx++;
+                        }
+                    }
                 }
             } else if (repeat.isChecked()) {
                 fileIdx = 0;
@@ -167,7 +217,13 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("Files", "Size: " + files.length);
                     if (files.length > 0) {
                         final Random r = new Random();
-                        String file = path + "/" + files[r.nextInt(files.length - 1)].getName();
+                        if (choseRandomly) {
+                            curFileIndex = r.nextInt(files.length - 1);
+                        } else {
+                            if (++curFileIndex >= files.length) fileIdx = 0;
+                        }
+                        String file = files[curFileIndex].getName();
+                        file = path + "/" + file;
                         return file;
                     }
                 } else {
